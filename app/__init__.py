20e34483a423b129edbc9135e30ee4a00789486e
Tarek Elsayed <60650661+tareknaser@users.noreply.github.com>
2026-06-16 22:46:44 -0700

    Add a photo of yourself to the website (#7)
    Closes #1

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,39 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
 
+# Pages shown in the nav bar (label + route endpoint)
+PAGES = [
+    {"name": "Home", "endpoint": "index"},
+    {"name": "Experience", "endpoint": "experience"},
+    {"name": "Hobbies", "endpoint": "hobbies"},
+    {"name": "Travel", "endpoint": "travel"},
+]
+
+
+@app.context_processor
+def inject_pages():
+    return {"pages": PAGES, "url": os.getenv("URL")}
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="Tarek Elsayed")
+
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html', title="Experience")
+
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html', title="Hobbies")
+
+
+@app.route('/travel')
+def travel():
+    return render_template('travel.html', title="Travel")
